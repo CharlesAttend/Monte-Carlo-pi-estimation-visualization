49c28c0a271a7d0fdee61a7070ab3a4d9dbe63f5
Charles <charlo22@hotmail.fr>
2021-08-08 17:50:32 +0200

    basic tailwind install

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        "dark_blue": "#C2DA74",
+        "light_blue" : "#8BD3DB",
+        "red" : "#E76F51",
+        "orange" : "#E9C46A",
+        "yellow" : "#F4A261"
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
