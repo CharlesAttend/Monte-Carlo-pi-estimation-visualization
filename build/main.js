81b3f8ee9af0280464a578b90d0b255e0710dacf
Charles <charlo22@hotmail.fr>
2021-08-08 21:52:22 +0200

    Some css, basing front

--- build/main.js
+++ build/main.js
@@ -1,11 +1,24 @@
 function setup() {
-    createCanvas(windowWidth,windowHeight);
+    let canevasDiv = document.querySelector('#canevas');
+    let w = canevasDiv.clientWidth;
+    let h = canevasDiv.clientHeight;
+    let canevas = createCanvas(w, h);
+    canevas.parent('canevas');
 }
 
 function draw() {
     background('#264653');
-    strokeWeight(5); 
+    strokeWeight(5);
     stroke('#E76F51');
-    ellipse(4*width/6, height/4 + width/8, width/4, width/4);
-    rect(width/6, height/4, width/4, width/4)
+    ellipse(4 * width / 6, height / 4 + width / 8, width / 4, width / 4);
+    rect(width / 6, height / 4, width / 4, width / 4);
+}
+
+
+function windowResized() {
+    let canevasDiv = document.querySelector('#canevas');
+    let w = canevasDiv.clientWidth;
+    let h = canevasDiv.clientHeight;
+    let canevas = createCanvas(w, h);
+    canevas.parent('canevas');
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,21 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    height: {
+      10: '10%',
+      90: '90%',
+      5: '5%',
+      screen: '100vh'
+    },
     extend: {
       colors: {
-        "dark_blue": "#C2DA74",
-        "light_blue" : "#8BD3DB",
-        "red" : "#E76F51",
-        "orange" : "#E9C46A",
-        "yellow" : "#F4A261"
+        "dark_blue": "#264653",
+        "light_blue": "#2A9D8F",
+        "red": "#E76F51",
+        "orange": "#E9C46A",
+        "yellow": "#F4A261"
       }
-    },
+    }
   },
   variants: {
     extend: {},
