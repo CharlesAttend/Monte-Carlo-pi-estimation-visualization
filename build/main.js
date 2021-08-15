9b5d678b3b2d681ca56355e50265f0e1c3edd191
Charles <charlo22@hotmail.fr>
2021-08-15 12:49:46 +0200

    better shape position

--- build/main.js
+++ build/main.js
@@ -1,24 +1,51 @@
+let isMobile;
+let rayon;
+
 function setup() {
-    let canevasDiv = document.querySelector('#canevas');
-    let w = canevasDiv.clientWidth;
-    let h = canevasDiv.clientHeight;
-    let canevas = createCanvas(w, h);
-    canevas.parent('canevas');
+	rectMode(CENTER);
+
+	let canevasDiv = document.querySelector('#canevas');
+	let w = canevasDiv.clientWidth;
+	let h = canevasDiv.clientHeight;
+	let canevas = createCanvas(w, h);
+	canevas.parent('canevas');
+	
+	isMobile = (windowWidth < 768) ? true : false;
+	rayon = isMobile ? width/2 : width/4;
+
+	background('#264653');
+	fill(229, 231, 235);
+	strokeWeight(5);
+	stroke('#E76F51');
+	if(isMobile){
+		ellipse(
+			width / 2,                         		// x
+			3 * height / 4,                 		// y
+			rayon);                                 // r
+
+		rect(width / 2,                             // x
+			height / 4,                             // y
+			rayon);                                 // r 
+	} else {
+		ellipse(
+			3 * width / 4,                          // x
+			height / 2,                     		// y
+			rayon);                                 // r
+
+		rect(width / 4,                             // x
+			height / 2,                             // y
+			rayon);                                 // r 
+	}
+}
+
+function windowResized() {
+	setup()
 }
 
 function draw() {
-    background('#264653');
-    strokeWeight(5);
-    stroke('#E76F51');
-    ellipse(4 * width / 6, height / 4 + width / 8, width / 4, width / 4);
-    rect(width / 6, height / 4, width / 4, width / 4);
+	
 }
 
+function waterDrop(speed){
 
-function windowResized() {
-    let canevasDiv = document.querySelector('#canevas');
-    let w = canevasDiv.clientWidth;
-    let h = canevasDiv.clientHeight;
-    let canevas = createCanvas(w, h);
-    canevas.parent('canevas');
 }
